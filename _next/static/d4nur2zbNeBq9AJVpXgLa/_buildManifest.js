a90e38c0adf7b0d55a120638734423a4dddf340a
kayoshi03 <r0n1ronov1@gmail.com>
2025-11-26 16:59:15 +0500

    Updates

--- _next/static/d4nur2zbNeBq9AJVpXgLa/_buildManifest.js
+++ _next/static/d4nur2zbNeBq9AJVpXgLa/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/gar//_next/:path+",
-        "destination": "/gar/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
